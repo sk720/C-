f5a5e25c2b4dc2c06a67eb3e1ac2e6c5682d8e9d
Sanjay kr <43911014+sk720@users.noreply.github.com>
2018-10-07 21:23:24 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+//Wap to addition of two number from user input
+// << -->insertion operator and >> -->this is extraction operator
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"Enter the value of a and b:";
+	cin>>a>>b;//read a and read b
+	c=a+b;
+	cout<<"Sum is: "<<c <<endl;
+	return 0;
+}
